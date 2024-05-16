12a80336a5d5b11b68f4970d8f6bb216d2fb3251
pgatundu <117097252+pgatundu@users.noreply.github.com>
2024-05-16 16:02:13 +0300

    2nd commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -33,5 +33,49 @@ def entry(request,title):
             "content":html_content
         })
 
+def search(request):
+    entry_search = request.POST['q']
+    html_content = convert_md_to_html(entry_search)
+    if html_content is not None:
+        return render(request,"encyclopedia/entry.html",{
+             "title":entry_search,
+             "content":html_content})
+    else:
+        allEntries = util.list_entries()
+        recommendation = []
+        for entry in allEntries:
+            if entry_search.lower() in entry.lower():
+                recommendation.append(entry)
+        return render(request,'encyclopedia/search.html',
+        {
+            'recommendation': recommendation
+        })
+
+def new_page(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        titleExist = util.get_entry(title)
+        if titleExist is not None:
+            return render( request, "encyclopedia/error.html",
+            {
+                "message": "Entry Page Exists"
+            })
+        else:
+            util.save_entry(title,content)
+            html_content = convert_md_to_html(title)
+            return render(request,"encyclopedia/entry.html",
+            {
+                "title":title,
+                "content":html_content
+            })
+
+
+
+
+
+
 
 
